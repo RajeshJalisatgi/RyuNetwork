90ee64e318139f4729000d3784b5584ed9d277f5
Ali <TheGreatest@hutchs-mbp.attlocal.net>
2017-12-04 08:33:24 -0800

    test phase

--- ryu/app/switch_custom.py
+++ ryu/app/switch_custom.py
@@ -23,18 +23,23 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
+from ryu.ofproto import ofproto_v1_2
 from ryu.lib.mac import haddr_to_bin
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
+from ryu.lib.packet import arp
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import icmp
+from ryu.lib import mac
 from ryu.lib.packet import ether_types
-from ryu.app.test_arp import RunTestMininet
 
 
-class SimpleSwitch(app_manager.RyuApp):
+
+class CustomSwitch(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
-        super(SimpleSwitch, self).__init__(*args, **kwargs)
+        super(CustomSwitch, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
 
     def add_flow(self, datapath, in_port, dst, actions):
